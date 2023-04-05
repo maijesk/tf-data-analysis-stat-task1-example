43854d7897d221a699f98a06eab9bb0e249c9db1
maijesk <109813087+maijesk@users.noreply.github.com>
2023-04-05 23:22:35 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,13 @@
 import pandas as pd
 import numpy as np
 
+chat_id = 700775408
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+def solution(samples: np.array) -> float:
+    sigma = np.sqrt(np.log(1 + (np.std(samples) / np.mean(samples))**2))
+    mu = np.log(np.mean(samples)) - sigma**2 / 2
+    a_est = np.exp(mu)
+    sample_mean = np.mean(samples)
+    mu_est_sample = np.log(sample_mean - 407) - sigma**2 / 2
+    a_est_sample = np.exp(mu_est_sample)  
+    return a_est_sample
