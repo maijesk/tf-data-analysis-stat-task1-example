fba1ddb3d4029bcbe5d6d7cf5efffe8ff148265d
maijesk <109813087+maijesk@users.noreply.github.com>
2023-04-06 20:56:01 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,10 +4,7 @@ import numpy as np
 chat_id = 700775408
 
 def solution(x: np.array) -> float:
-    sigma = np.sqrt(np.log(1 + (np.std(x) / np.mean(x))**2))
-    mu = np.log(np.mean(x - 407)) - sigma**2 / 2
-    a_est = np.exp(mu)
-    x_mean = np.mean(x)
-    mu_est_x = np.log(x_mean - 407) - sigma**2 / 2
-    a_est_x = np.exp(mu_est_x)
-    return a_est_x
+    log_x = np.log(x)
+    sigma_sq = np.var(log_x)
+    mean_lognormal = np.mean(np.log(x)) - sigma_sq/2 - 407
+    return mean_lognormal
