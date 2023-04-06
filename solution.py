9e4773ea91820f9f47b3f5ab2881f14ff683b231
maijesk <109813087+maijesk@users.noreply.github.com>
2023-04-06 23:12:17 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,10 +1,7 @@
 import pandas as pd
 import numpy as np
 
-chat_id = 700775408
+chat_id = 700775408 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array) -> float:
-    log_x = np.log(x)
-    sigma_sq = np.var(log_x)
-    mean_lognormal = np.mean(np.log(x)) - sigma_sq/2 - 407
-    return mean_lognormal
+def solution(x: np.array) -> float:  
+    return np.log(x-407).mean()
